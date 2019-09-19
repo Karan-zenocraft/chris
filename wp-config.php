939634ec4403cdeabe09f2fb86f203de26358b96
root <root@localhost.creatusello.com>
2019-09-19 17:26:09 +0530

    add database configurations of live

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'chris');
+define('DB_NAME', 'zadmin_gallagherchris');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'gallagherchris');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rutusha@123');
+define('DB_PASSWORD', 'yza6a8ygu');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
