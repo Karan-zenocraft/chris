f8608cfe86ba9257c3d8ec32e2fae1f378f383a0
root <root@localhost.creatusello.com>
2019-09-24 14:42:14 +0530

    homepage responsive

--- wp-content/themes/ashe-child/archive-stories.php
+++ wp-content/themes/ashe-child/archive-stories.php
@@ -4,9 +4,9 @@ href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <div class="row story_list_row">
 	<?php if (have_posts()): while (have_posts()): the_post();?>
-				<div class="col-md-6 story_image">	<?php the_post_thumbnail();?></div> <div
-				class="col-md-6"><strong><h3><?php the_title();?></h3> </strong><?php echo '<div
-				class="entry-content">';
+						<div class="col-md-6 story_image">	<?php the_post_thumbnail();?></div> <div
+						class="col-md-6 story_description"><strong><h3><?php the_title();?></h3> </strong><?php echo '<div
+						class="entry-content">';
         the_content();
         echo '</div>';
         ?></div>																									<?php endwhile;endif;?>
