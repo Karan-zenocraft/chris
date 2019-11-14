298d8207c7ec2c9205dbb426a0a18ef78ff21242
root <root@localhost.creatusello.com>
2019-11-14 12:39:55 +0530

    homepage changes

--- wp-content/themes/ashe-child/homepage.php
+++ wp-content/themes/ashe-child/homepage.php
@@ -57,43 +57,41 @@ if ($sliderQuery->have_posts()):
         } else {
             $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full', false);
         }
-        ?>
-																																																																																																															<div class="slider-item">
+        ?>																																																																																												  <div class="slider-item">
+								<div class="slider-item-bg" style="background-image:url(<?php echo esc_url($featured_image[0]); ?>);"></div>
 
-																																																																																																																<div class="slider-item-bg" style="background-image:url(<?php echo esc_url($featured_image[0]); ?>);"></div>
+																																																																																																																								<div class="cv-container image-overlay">
+																																																																																																																									<div class="cv-outer">
+																																																																																																																										<div class="cv-inner">
+																																																																																																																											<div class="slider-info">
 
-																																																																																																																<div class="cv-container image-overlay">
-																																																																																																																	<div class="cv-outer">
-																																																																																																																		<div class="cv-inner">
-																																																																																																																			<div class="slider-info">
+																																																																																																																												<?php $category_list = get_the_category_list(', ');?>
 
-																																																																																																																				<?php $category_list = get_the_category_list(', ');?>
+																																																																																																																												<?php if ($category_list): ?>
+																																																																																																																												<div class="slider-categories">
+																																																																																																																													<?php echo '' . $category_list; ?>
+																																																																																																																												</div>
+																																																																																																																												<?php endif;?>
 
-																																																																																																																				<?php if ($category_list): ?>
-																																																																																																																				<div class="slider-categories">
-																																																																																																																					<?php echo '' . $category_list; ?>
-																																																																																																																				</div>
-																																																																																																																				<?php endif;?>
+																																																											<h2 class="slider-title">
+																																																											<a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title();?></a>
+																																																											</h2>
 
-																																																							<h2 class="slider-title">
-																																																							<a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title();?></a>
-																																																							</h2>
+																																																											<div class="slider-content"><?php ashe_excerpt(30);?></div>
 
-																																																							<div class="slider-content"><?php ashe_excerpt(30);?></div>
+																																																											<div class="slider-read-more">
+																																																											<a href="<?php echo esc_url(get_permalink()); ?>" target="_blank"><?php esc_html_e('read more', 'ashe');?></a>
+																																																											</div>
 
-																																																							<div class="slider-read-more">
-																																																							<a href="<?php echo esc_url(get_permalink()); ?>" target="_blank"><?php esc_html_e('read more', 'ashe');?></a>
-																																																							</div>
+																																																											<div class="slider-date"><?php the_time(get_option('date_format'));?></div>
 
-																																																							<div class="slider-date"><?php the_time(get_option('date_format'));?></div>
+																																																											</div>
+																																																											</div>
+																																																											</div>
+																																																											</div>
 
-																																																							</div>
-																																																							</div>
-																																																							</div>
-																																																							</div>
-
-																																																							</div>
-																																																							<?php
+																																																											</div>
+																																																											<?php
 endwhile; // Loop end
 endif;
 
